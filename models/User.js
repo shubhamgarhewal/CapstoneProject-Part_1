f6ef3a4422f720ef5b10e4a00127279de204873c
Shubham Garhewal <96170508+shubhamgarhewal@users.noreply.github.com>
2022-08-16 22:37:58 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    contactnum: {
+      type: Number,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", UserSchema);
