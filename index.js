717024887336a42ac935c238634b056514c60e32
Shubham Garhewal <96170508+shubhamgarhewal@users.noreply.github.com>
2022-08-16 22:29:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoute = require("./controllers/auth");
+const shippingRoute = require("./controllers/Shipping.Address");
+const productRoute = require("./controllers/Product");
+const cors = require("cors");
+const orderRoute = require("./controllers/Order");
+const app = express();
+const PORT = 8000;
+
+mongoose
+  .connect("mongodb://localhost:27017/upgrad-project")
+  .then(() => {
+    console.log("DB Connect");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(express.json());
+
+app.use("/", authRoute);
+app.use(shippingRoute);
+app.use("/products", productRoute);
+app.use("/orders", orderRoute);
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log("backend is running! " + PORT);
+});
